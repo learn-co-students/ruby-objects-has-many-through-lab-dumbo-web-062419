7dc7813e2d8c73fd3dde977220f3da57713b6c2c
github username <github email address>
2019-06-28 18:25:04 -0400

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,25 @@
+
+class Genre
+    attr_reader :name, :genre
+    attr_accessor :song
+    
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+   
+    def songs
+        Song.all.select { |songs| songs.genre == self }
+    end
+
+    def artists
+        songs.map {|song| song.artist }
+    end
+
+end
